258f06d7f023778f4e5b3e8776d386396496ea35
Sunil Munikar <sunilmunikar@hotmail.com>
2013-08-27 23:45:23 +0200

    * pluggable asp.net project created * forum added as pluggin/area in core project * fluentauthentication demo created

---
+++ Core/Areas/FluentsecurityDemo/Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using FluentSecurity;
+using FluentsecurityDemo.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FluentsecurityDemo
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleSecurityAttribute(), 0);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            // Use LocalDB for Entity Framework by default
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+
+            SecurityConfigurator.Configure(configuration =>
+            {
+                // Tell FluentSecurity where to obtain the user authentication status from
+                configuration.GetAuthenticationStatusFrom(() =>
+                    HttpContext.Current.User.Identity.IsAuthenticated);
+
+                configuration.For<HomeController>().Ignore();
+
+                // Make sure that users can still log on
+                configuration.For<AccountController>(ac => ac.LogOn()).Ignore();
+                configuration.For<AccountController>(ac => ac.ChangePassword()).RequireRole("Administrator");
+            });
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
