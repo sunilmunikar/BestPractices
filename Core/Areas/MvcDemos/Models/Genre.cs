14461c87a6b1bae0057aaa18e8a8858ba759378a
Sunil Munikar <sunilmunikar@hotmail.com>
2013-09-08 17:30:04 +0200

    * Implemented Genere => Name required

--- Core/Areas/MvcDemos/Models/Genre.cs
+++ Core/Areas/MvcDemos/Models/Genre.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcDemos.Models
 {
     public partial class Genre
     {
         public int GenreId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Album> Albums { get; set; }
