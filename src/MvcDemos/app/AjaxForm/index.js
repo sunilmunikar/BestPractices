ef7cbe8dfa07f596ddc02299d7b77ae3dc9a20eb
Sunil Munikar <sunilmunikar@hotmail.com>
2013-11-16 13:24:49 +0100

    * closure based jqueryUi dialog

--- src/MvcDemos/app/AjaxForm/index.js
+++ src/MvcDemos/app/AjaxForm/index.js
@@ -1,5 +1,6 @@
-﻿'use strict';
-var index = (function() {
+﻿var index = (function () {
+    'use strict';
+
     // Internal members
     var iffeId = 'index';
 
@@ -15,7 +16,7 @@ var index = (function() {
 
         $("#create-user")
             .button()
-            .click(function() {
+            .click(function () {
                 $("#dialog-form").dialog("open");
             });
     }
@@ -24,3 +25,30 @@ var index = (function() {
 
     //#endregion
 })();
+
+//http://www.kendoui.com/blogs/teamblog/posts/13-11-12/how-to-do-javascript-alerts-without-being-a-jerk.aspx
+window.JqueryDialog = (function () {
+
+    //create modal window on the fly
+
+    var win = $("<div>").dialog({
+        buttons: [{
+            text: "Ok",
+            click: function () {
+                $(this).dialog("close");
+            }
+        }],
+        autoOpen: false,
+        closeOnEscape:true
+    });
+
+    return function (msg, deactivate) {
+        if (deactivate) {
+            win.bind("dialogclose", deactivate);
+        } else
+            win.unbind("dialogclose", deactivate);
+
+        win.dialog("open");
+    };
+
+}());
