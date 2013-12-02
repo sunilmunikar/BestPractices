717a3f81611adb1effdaf8c4cca8d019cf02a052
Sunil Munikar <sunilmunikar@hotmail.com>
2013-12-02 20:50:44 +0100

    jqgrid added

--- src/MvcDemos/App_Start/BundleConfig.cs
+++ src/MvcDemos/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace MvcDemos
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
+                        "~/Scripts/jquery.jqGrid*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
