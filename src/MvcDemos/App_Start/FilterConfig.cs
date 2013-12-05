c91865c6b89a28cc11720821919c0427bdf1b9b5
Sunil Munikar <sunilmunikar@hotmail.com>
2013-12-05 21:19:24 +0100

    * custom error handeling

--- src/MvcDemos/App_Start/FilterConfig.cs
+++ src/MvcDemos/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace MvcDemos
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
             filters.Add(new HandleSecurityAttribute(), 0);
         }
     }
