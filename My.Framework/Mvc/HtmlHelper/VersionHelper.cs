5bcc4453220c85ab8a776a7616256ef6dc583549
Sunil Munikar <sunilmunikar@hotmail.com>
2013-12-28 18:56:21 +0100

    * implemented versioning

---
+++ My.Framework/Mvc/HtmlHelper/VersionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace My.Framework.Mvc.HtmlHelper
+{
+    public static class VersionHelper
+    {
+        private static readonly Lazy<string> Version = new Lazy<string>(() =>
+        {
+            try
+            {
+                System.Version version = Assembly.GetExecutingAssembly().GetName().Version;
+                return String.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+            }
+            catch (Exception)
+            {
+                return "?.?.?.?";
+            };
+        });
+
+        /// <summary>
+        /// Returns the Current Version from the AssemblyInfo.cs file.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <returns></returns>
+        public static string CurrentVersion(this System.Web.Mvc.HtmlHelper helper)
+        {
+            return Version.Value;
+        }
+    }
+}
