bf0d2d79ac05f0c1333b47f69f7a7ebfa6313d45
Sunil Munikar <sunilmunikar@hotmail.com>
2014-10-01 21:44:09 +0200

    * added new webapi project

---
+++ src/WebApiDemos/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiDemos.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/WebApiDemos/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiDemos.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
