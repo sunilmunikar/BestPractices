b870df3b57f8baca999a8c9599726192050ee6ca
Sunil Munikar <sunilmunikar@hotmail.com>
2015-01-24 22:03:02 +0100

    instantiate the javascript variable from  backend

---
+++ src/MvcDemos/Features/Setting/SettingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemos.Features.Setting
+{
+    public class SettingViewModel
+    {
+        public bool IsDebug { get; set; }
+    }
+
+    public class SettingController : Controller
+    {
+        public ActionResult Index()
+        {
+            var settingVM = new SettingViewModel()
+            {
+                IsDebug = true
+            };
+
+            return View(settingVM);
+        }
+    }
+}
---
+++ src/MvcDemos/Helpers/JavaScriptHelper.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemos.Helpers
+{
+    public static class JavaScriptHelper
+    {
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+
+        public static IHtmlString Json(this HtmlHelper helper, object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[]
+				{
+					new StringEnumConverter(),
+				},
+                StringEscapeHandling = StringEscapeHandling.EscapeHtml
+            };
+
+            return MvcHtmlString.Create(JsonConvert.SerializeObject(obj, settings));
+        }
+    }
+}
