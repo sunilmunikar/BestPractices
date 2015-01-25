69083147d6821f752121e4b1e709026aa5504320
Sunil Munikar <sunilmunikar@hotmail.com>
2015-01-25 20:19:04 +0100

    angularj validation using ng-messages

---
+++ src/MvcDemos/app/user.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('User', User);
+
+    User.$inject = ['$location'];
+
+    function User($location) {
+        this.data = {};
+
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'validation sample ';
+        vm.submit = submit;
+
+        activate();
+
+        function activate() { }
+
+        function submit(valid) {
+            if (!valid) return;
+            console.log(valid);
+        }
+    }
+})();
