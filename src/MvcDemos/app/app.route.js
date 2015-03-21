b06b3300a8b1c4e45bcf6420762744f203714da3
Sunil Munikar <sunilmunikar@hotmail.com>
2015-03-21 19:16:22 +0100

    Uirouter nested view example

--- src/MvcDemos/app/app.route.js
+++ src/MvcDemos/app/app.route.js
@@ -9,11 +9,20 @@
                 controller: 'Reservation',
                 controllerAs: 'vm'
             })
-            .state('reservationServerSideOnly', {
-                url: '/reservationServerSideOnly',
-                templateUrl: 'app/reservation/reservationServerSideOnly.html',
-                controller: 'Reservation',
-                controllerAs: 'vm'
+            .state('reservation.ClientSide', {
+                url: '/ClientSide',
+                templateUrl: 'app/reservation/reservationClientSide.html'
+                //,
+                //controller: 'Reservation',
+                //controllerAs: 'vm'
+            })
+
+            .state('reservation.ServerSideOnly', {
+                url: '/ServerSideOnly',
+                templateUrl: 'app/reservation/reservationServerSideOnly.html'
+                //,
+                //controller: 'Reservation',
+                //controllerAs: 'vm'
             });
 
         $urlRouterProvider.otherwise('/');
