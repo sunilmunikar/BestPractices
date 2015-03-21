5c50366628f89cd8c60fe2b17d0cc0ccb56b5273
Sunil Munikar <sunilmunikar@hotmail.com>
2015-03-21 20:44:24 +0100

    configure  BrowserSync

---
+++ src/MvcDemos/gulpfile.js
@@ -0,0 +1,30 @@
+﻿var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+
+// Static server
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// or...
+
+//gulp.task('browser-sync', function () {
+//    browserSync({
+//        proxy: "yourlocal.dev"
+//    });
+//});
+
+
+// use default task to launch BrowserSync and watch JS files
+gulp.task('default', ['browser-sync'], function () {
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("./app/**/*.*").on("change", browserSync.reload);
+
+});
