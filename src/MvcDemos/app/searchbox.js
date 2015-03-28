c67be3dbb01886183927e9ae404041aefc05b9d1
Sunil Munikar <sunilmunikar@hotmail.com>
2015-03-28 18:10:50 +0100

    searchbox directive added

---
+++ src/MvcDemos/app/searchbox.js
@@ -0,0 +1,34 @@
+﻿(function (app) {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('searchBox', searchBox)
+
+    function searchBox() {
+        //<search-box search-field="music" search-term="whatever"></search-box>
+
+        return {
+            restrict: 'EA',
+            scope: {
+                searchField: '@',
+                searchValue: '=searchTerm'
+            },
+            controller: SearchCtrl,
+            controllerAs: 'vm',
+            bindToController: true,
+            template: '<div class="input-control text"><input type="text" ng-model="vm.searchValue"></input><button class="btn-search"></button></div>{{vm}}',
+            link: function (scope, element, attrs, ctrl) {
+                element.bind('click', function () {
+                    console.log(ctrl.search());
+                })
+            }
+        }
+
+        function SearchCtrl() {
+            SearchCtrl.prototype.search = function () {
+                return 'Searching for"' + this.searchValue + ' on field ' + this.searchField + '"...';
+            }
+        }
+    }
+})();
